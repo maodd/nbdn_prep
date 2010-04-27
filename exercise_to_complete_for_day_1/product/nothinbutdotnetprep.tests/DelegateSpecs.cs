48aa40678986040b5d52dfe2fb1cc2e7f8b6ac0c
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-04-27 11:42:06 -0600

    Adding the delegate exercise

---
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep.tests/DelegateSpecs.cs
@@ -0,0 +1,46 @@
+ 
+using System;
+using developwithpassion.bdd.contexts;
+using developwithpassion.bdd.harnesses.mbunit;
+using developwithpassion.bdddoc.core;
+
+namespace nothinbutdotnetprep.tests
+{
+    public class DelegateSpecs
+    {
+        public abstract class concern : observations_for_a_static_sut
+        {
+
+        }
+
+        [Concern(typeof (Delegate))]
+        public class when_asked_to_rethrow_all_exceptions_as_argumentexceptions : concern
+        {
+            context c = () =>
+            {
+                inner_exception = new NotImplementedException();
+
+                Action do_something = delegate
+                {
+                    throw inner_exception;
+                };
+            };
+
+            because b = () =>
+            {
+
+                result =new Running(do_something).get_resulting_exception();
+            };
+
+
+            it should_throw_any_resulting_exception_as_an_argument_exception = () =>
+            {
+               result.should_be_an_instance_of<ArgumentException>()
+                   .InnerException.should_be_equal_to(inner_exception);
+            };
+
+            static Exception result;
+            static Exception inner_exception;
+        }
+    }
+}
